28fe47124d95eef81e25688c789696c1f77c4fbe
wassimhajji <wassim.hajji2@gmail.com>
2022-01-19 17:59:57 +0100

    I changed the consumers for the hardware and the streams, I made a background service just like you have done in the github project you added me in I still didnt go for the parse and the store in database I will do it first thing tomorrow

--- AnalyticsServer/RmqServer/SlaveExchangeStreams.cs
+++ AnalyticsServer/RmqServer/SlaveExchangeStreams.cs
@@ -9,21 +9,21 @@ namespace AnalyticsServer.RmqServer
         public static void Consume(IModel channel)
         {
             channel.ExchangeDeclare("SlaveExchange", ExchangeType.Topic);
-            channel.QueueDeclare("SlaveExchange",
+            channel.QueueDeclare("StreamsQueue",
                 durable: true,
                 exclusive: false,
                 autoDelete: false,
                 arguments: null);
-            channel.QueueBind("SlaveExchange", "SlaveExchange", "Slave.Stream.Key");
+            channel.QueueBind("StreamsQueue", "SlaveExchange", "Slave.Stream.Key");
             channel.BasicQos(0, 100, false);
-            var StreamsQueue = new Queue<string>();
+            
 
             var consumer = new EventingBasicConsumer(channel);
             consumer.Received += (sender, e) =>
             {
                 var body = e.Body.ToArray();
                 var message = Encoding.UTF8.GetString(body);
-                StreamsQueue.Enqueue(message);
+                
             };
         }
     }
---
+++ AnalyticsServer/RmqServer/SlaveStreamsConsumer.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace AnalyticsServer.RmqServer
+{
+    public class SlaveStreamsConsumer : BackgroundService
+    {
+        private readonly string exchangeName = "SlaveExchange";
+        private readonly string queueName = "HWConsumer";
+        private readonly Dictionary<string, object> args = new Dictionary<string, object>();
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var connection = GetConnection();
+            var channel = connection.CreateModel();
+            var message = new object();
+            channel.ExchangeDeclare(exchangeName, ExchangeType.Topic);
+            channel.QueueDeclare(queueName, true, false, true, args);
+            channel.QueueBind(queueName, exchangeName, "Slave.HW.Key");
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (sender, e) =>
+                {
+                    var body = e.Body.ToArray();
+                    message = JsonConvert.DeserializeObject(Encoding.UTF8.GetString(body));
+                    channel.BasicConsume(queueName, true, consumer);
+
+                };
+                consumer.Shutdown += OnConsumerShutdown;
+                consumer.Registered += OnConsumerRegistered;
+                consumer.Unregistered += OnConsumerUnregistered;
+                consumer.ConsumerCancelled += OnConsumerConsumerCancelled;
+
+                await Task.Delay(3000, stoppingToken);
+            }
+
+            
+
+        }
+        private IConnection GetConnection()
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://WQK56CEwyKn7Dgpp:Mev9LAc9uP8AK8FK@20.188.60.149:5672");
+            return factory.CreateConnection();
+        }
+        private void OnConsumerConsumerCancelled(object sender, ConsumerEventArgs e) { }
+        private void OnConsumerUnregistered(object sender, ConsumerEventArgs e) { }
+        private void OnConsumerRegistered(object sender, ConsumerEventArgs e) { }
+        private void OnConsumerShutdown(object sender, ShutdownEventArgs e) { }
+        private void RabbitMQ_ConnectionShutdown(object sender, ShutdownEventArgs e) { }
+    } 
+}
