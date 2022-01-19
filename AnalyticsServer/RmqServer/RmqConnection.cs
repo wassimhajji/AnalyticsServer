bd3033cb765921aa4c20a91a270d313875080bf5
wassimhajji <wassim.hajji2@gmail.com>
2022-01-19 12:49:31 +0100

    second commit: added the consumer for the slave exchange hardware, the messages are stored in a queue HWQueue, created a class for the connection and a class for the slave exchange hardware to cionsume

---
+++ AnalyticsServer/RmqServer/RmqConnection.cs
@@ -0,0 +1,20 @@
+﻿using RabbitMQ.Client;
+
+namespace AnalyticsServer.RmqServer
+{
+    public class RmqConnection
+    {
+        static void Connection()
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://WQK56CEwyKn7Dgpp:Mev9LAc9uP8AK8FK@20.188.60.149:5672")
+            };
+            using var Connection = factory.CreateConnection();
+            using var channel = Connection.CreateModel();
+            SlaveExchangeHW.Consume(channel);   
+
+
+        }
+    }
+}
